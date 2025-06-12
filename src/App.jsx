463102cf7ae5b09991d2c78de41cf64d35008f3c
Atharvashrote <atharvashrote1126@gmail.com>
2025-06-12 13:23:40 +0530

    admin dashboardn UI  is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Login from './componenet/Auth/Login'
 import EmployeeDashboard from './componenet/dashboard/EmployeeDashboard'
+import AdminDashboard from './componenet/dashboard/AdminDashboard'
 
 
 const App = () => {
   return (
     <>
     {/*<Login />*/}
-    <EmployeeDashboard />
+    {/*<EmployeeDashboard />*/}
+    <AdminDashboard />
    
     </>
   )
